40f22154a692b73ad85bd991c0cbe2da8b92e943
Fernando Poncio <fecrackle@gmail.com>
2020-05-25 23:51:26 -0600

    Creando Category y ListCategories con sus estilos

--- src/components/Category/styles.js
+++ src/components/Category/styles.js
@@ -12,9 +12,8 @@ export const Image = styled.img`
   border: 1px solid #ddd;
   box-shadow: 0px 10px 14px rgba(0, 0, 0, .2);
   border-radius: 50%;
-  height: auto;
+  height: 75px;
   overflow: hidden;
   object-fit: cover;
-  height: 75px;
   width: 75px;
 `
