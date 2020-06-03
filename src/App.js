539805f73f6702ed12e90491cbe1e5cab2bb4fa4
Fernando Poncio <fecrackle@gmail.com>
2020-06-03 00:03:17 -0600

    Agregando Logo SVG

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { ListOfCategories } from './components/ListOfCategories'
 
 import { GlobalStyle } from './GlobalStyles'
+import { Logo } from './components/Logo'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { ListOfCategories } from './components/ListOfCategories'
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
