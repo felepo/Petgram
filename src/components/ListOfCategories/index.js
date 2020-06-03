0f880db1388b7813e4302b7c4f266a8c23098d3a
Fernando Poncio <fecrackle@gmail.com>
2020-06-02 23:10:40 -0600

    Agregando componentes de PhotoCard y ListOfPhotoCard

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -8,9 +8,11 @@ import { categories } from '../../../api/db.json'
 export const ListOfCategories = () => {
   return (
     <List>
-      {
-        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
-      }
+      {categories.map((category) => (
+        <Item key={category.id}>
+          <Category {...category} />
+        </Item>
+      ))}
     </List>
   )
 }
