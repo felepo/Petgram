65673be66bafce6f5fce94ce927f64be8b0495ce
Fernando Poncio <fecrackle@gmail.com>
2020-06-08 22:42:26 -0600

    Agregando keyframes en imagenes principales y reordenando estilos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { ListOfCategories } from './components/ListOfCategories'
